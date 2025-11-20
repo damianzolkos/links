e46394094bd041a9aa2f73ce778705d4818903aa
Damian Żółkoś <damianzolkos@gmail.com>
2025-11-20 21:45:13 +0100

    Arts added, css fixes

--- main.js
+++ main.js
@@ -27,11 +27,16 @@ const menuToggle = document.getElementById("menuToggle");
 const sideMenu = document.getElementById("sideMenu");
 const overlay = document.getElementById("overlay");
 const clearBtn = document.getElementById("clearBtn");
+const artName = document.getElementById("artName");
+const randomArt = document.getElementById("randomArt");
 
 // hero and clock elements
-const searchInput = document.getElementById("searchInput");
 const hero = document.getElementById("hero");
 const clock = document.getElementById("clock");
+const searchInput = document.getElementById("searchInput");
+const art = document.getElementById("art");
+const help = document.getElementById("help");
+const topContainer = document.getElementById("topcontainer");
 
 let data = null;
 let currentData = null;
@@ -41,6 +46,92 @@ let linkToDelete = { groupIndex: null, linkIndex: null };
 let renderItemActions = true;
 
 let selected = undefined;
+
+const helpText = `
+'.' show all bookmarks
+' ' search the web
+`;
+
+const artSettings = {
+    randomArt: true,
+    images: [
+        {
+            name: "pink-cars",
+            url: "https://w.wallhaven.cc/full/k8/wallhaven-k881zd.jpg",
+        },
+        {
+            name: "blue-computers",
+            url: "https://w.wallhaven.cc/full/7j/wallhaven-7jp81o.jpg",
+        },
+        {
+            name: "japan-minimini",
+            url: "https://w.wallhaven.cc/full/e8/wallhaven-e88yjo.jpg",
+        },
+        {
+            name: "compic-painting",
+            url: "https://w.wallhaven.cc/full/9o/wallhaven-9oozpx.png",
+        },
+        {
+            name: "astronaut",
+            url: "https://w.wallhaven.cc/full/qr/wallhaven-qrrlzr.jpg",
+        },
+    ],
+};
+
+function loadArtFromLocal() {
+    const savedArtSettings = localStorage.getItem("artSettings");
+    if (savedArtSettings) {
+        let deserialized = JSON.parse(savedArtSettings);
+        setArt(deserialized.artName, deserialized.random, false);
+        artName.value = deserialized.artName;
+        randomArt.checked = deserialized.random;
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    artSettings.images.forEach((image) => {
+        const option = document.createElement("option");
+        option.value = image.name;
+        option.textContent = image.name;
+        artName.appendChild(option);
+    });
+    loadArtFromLocal();
+});
+
+artName.addEventListener("change", () => {
+    setArt(artName.value, false, true);
+    randomArt.checked = false;
+});
+
+randomArt.addEventListener("change", () => {
+    setArt(artName.value, true, true);
+});
+
+function saveArtSettingsToLocal() {
+    localStorage.setItem(
+        "artSettings",
+        JSON.stringify({ artName: artName.value, random: randomArt.checked }),
+    );
+}
+
+const setArt = (artName, random, save) => {
+    const img = document.createElement("img");
+
+    if (random) {
+        img.src =
+            artSettings.images[
+                Math.floor(Math.random() * artSettings.images.length)
+            ].url;
+    } else {
+        if (!artName) return;
+        img.src = artSettings.images.filter((x) => x.name === artName)[0].url;
+    }
+
+    art.innerHTML = "";
+    art.appendChild(img);
+
+    if (save) saveArtSettingsToLocal();
+};
 
 // === Toggle Menu ===
 menuToggle.addEventListener("click", () => {
@@ -77,7 +168,7 @@ function navigateLinks(direction) {
     if (!selected || direction === 0) {
         // Handle the case where no element is initially highlighted
         selected = links[0]; // Select the first link
-        selected.classList.add("highlighted");
+        if (selected) selected.classList.add("highlighted");
         return;
     }
 
@@ -96,13 +187,13 @@ function navigateLinks(direction) {
 
     // Update selected and add highlighted class to the new element
     selected = links[nextIndex];
-    selected.classList.add("highlighted");
-
-    // Optional: Scroll the selected element into view
-    selected.scrollIntoView({
-        behavior: "smooth",
-        block: "nearest",
-    });
+    if (selected) {
+        selected.classList.add("highlighted");
+        selected.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+        });
+    }
 }
 
 document.addEventListener("keydown", (e) => {
@@ -162,24 +253,36 @@ loadBookmarksBtn.addEventListener("click", () => bookmarksFile.click());
 bookmarksFile.addEventListener("change", handleBookmarksUpload);
 
 function showBookmarks() {
-    container.classList.remove("hidden");
+    container.style.display = "block";
     clock.style.display = "none";
+    art.style.display = "none";
+    help.style.display = "none";
     hero.classList.remove("hero-top");
+    topContainer.classList.remove("top-container");
 }
 
 function hideBookmarks() {
-    container.classList.add("hidden");
+    container.style.display = "none";
     clock.style.display = "block";
+    art.style.display = "flex";
+    help.style.display = "flex";
     hero.classList.add("hero-top");
+    topContainer.classList.add("top-container");
 }
 
 function toggleBookmarks() {
     container.classList.contains("hidden") ? showBookmarks() : hideBookmarks();
 }
 
-// When page first loads, hide the bookmarks grid
 window.addEventListener("DOMContentLoaded", () => {
     hideBookmarks();
+
+    // art
+
+    setArt("", false);
+
+    // help
+    help.innerHTML = `<code>${helpText.trim()}</code>`;
 });
 
 function handleBookmarksUpload(e) {
@@ -296,7 +399,7 @@ function saveToLocal() {
 }
 
 /* === Render Groups === */
-function renderGroups(data, { expandAll = false } = {}) {
+function renderGroups(data, { expandAll = false, showEmpty = false } = {}) {
     container.innerHTML = "";
 
     if (!data || !data.groups || !data.groups.length === 0) {
@@ -338,10 +441,10 @@ function renderGroups(data, { expandAll = false } = {}) {
         container.innerHTML = "";
 
         flatGroups.forEach((group, gIndex) => {
+            if (group.links.length === 0 && !showEmpty) return;
             const visible = isVisible(group);
             const section = document.createElement("section");
             section.classList.add("group");
-            section.dataset.depth = group._depth;
             section.style.display = visible ? "" : "none";
 
             // HEADER
@@ -359,6 +462,7 @@ function renderGroups(data, { expandAll = false } = {}) {
             if (hasChildren) {
                 const open = openSet.has(group);
                 toggle.textContent = open ? "▼" : "▶";
+                toggle.style.opacity = "0.3";
                 header.addEventListener("click", () => {
                     if (openSet.has(group)) {
                         // close group and all descendants
@@ -389,6 +493,13 @@ function renderGroups(data, { expandAll = false } = {}) {
             }
 
             header.appendChild(h2);
+            if (renderItemActions) {
+                const addBtn = document.createElement("button");
+                addBtn.classList.add("remove-btn", "remove-x");
+                addBtn.textContent = "+";
+                addBtn.addEventListener("click", () => openLinkModal(group));
+                header.appendChild(addBtn);
+            }
             if (deleteBtn) {
                 header.appendChild(deleteBtn);
             }
@@ -423,14 +534,6 @@ function renderGroups(data, { expandAll = false } = {}) {
                     ul.appendChild(li);
                 });
                 section.appendChild(ul);
-            }
-
-            if (renderItemActions) {
-                const addBtn = document.createElement("button");
-                addBtn.classList.add("add-btn");
-                addBtn.textContent = "+ Add link";
-                addBtn.addEventListener("click", () => openLinkModal(group));
-                section.appendChild(addBtn);
             }
 
             container.appendChild(section);
@@ -540,7 +643,10 @@ function handleSearch(query) {
     }
 
     showBookmarks();
-    renderGroups(filtered, { expandAll: true });
+    renderGroups(filtered, {
+        expandAll: true,
+        showEmpty: query.startsWith("."),
+    });
     navigateLinks(0);
     renderItemActions = true;
 }
